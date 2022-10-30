0d19a2ab2668b6bab6f93ffbf2498f2e51e26747
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-10-30 10:47:41 +0530

    Set up Api Service

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { setToken, call } from "./services/api";
 
 const App = () => {
+  useEffect(() => {
+    async function getResults() {
+      const result = await call("post", "auth/login", {
+        username: "username",
+        password: "password",
+      });
+
+      console.log(result);
+    }
+
+    getResults();
+  }, []);
   return <div>App</div>;
 };
 
